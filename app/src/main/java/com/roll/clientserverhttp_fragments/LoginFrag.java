b6e602db9c9cdd376661ee898554389b877bcb5b
Denys Romanchenko <6104172@gmail.com>
2017-03-04 18:00:43 +0200

    save_8

--- app/src/main/java/com/roll/clientserverhttp_fragments/LoginFrag.java
+++ app/src/main/java/com/roll/clientserverhttp_fragments/LoginFrag.java
@@ -215,40 +215,21 @@ public class LoginFrag extends Fragment implements View.OnClickListener {
             Auth auth = new Auth(login, pass);
             String jsonBody = gson.toJson(auth);
 
-            MediaType mediaType = MediaType.parse("application/json; charset=utf-8");
-            RequestBody body = RequestBody.create(mediaType, jsonBody);
-
-            Request request = new Request.Builder()
-                    .url(HttpProvider.BASE_URL + "/registration")
-                    .post(body)
-                    .build();
-
-            OkHttpClient client = new OkHttpClient();
-            client.setReadTimeout(15, TimeUnit.SECONDS);
-            client.setConnectTimeout(15, TimeUnit.SECONDS);
-
             try {
-                Response response = client.newCall(request).execute();
-                if (response.code() < 400) {
-                    String jsonResponse = response.body().string();
-                    Log.d("REGISTRATION", jsonResponse);
-                    AuthResponse authResponse = gson.fromJson(jsonResponse, AuthResponse.class);
-                    SharedPreferences sPref = context.getSharedPreferences("AUTH", Context.MODE_PRIVATE);
-                    SharedPreferences.Editor editor = sPref.edit();
-                    editor.putString("TOKEN", authResponse.getToken());
-                    editor.putString("LOGIN", login);
-                    editor.putString("PASS", pass);
-                    editor.commit();
-                } else if (response.code() == 409) {
-                    result = "User already exist!";
-                } else {
-                    String jsonResponse = response.body().string();
-                    Log.d("REGISTRATION ERROR", jsonResponse);
-                    result = "Server ERROR!";
-                }
+                String jsonResponse = HttpProvider.getInstance().registration(jsonBody);
+                AuthResponse authResponse = gson.fromJson(jsonResponse, AuthResponse.class);
+                SharedPreferences sPref = context.getSharedPreferences("AUTH", Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sPref.edit();
+                editor.putString("TOKEN", authResponse.getToken());
+                editor.putString("LOGIN", login);
+                editor.putString("PASS", pass);
+                editor.commit();
             } catch (IOException e) {
                 e.printStackTrace();
                 result = "Connection ERROR!";
+            } catch (Exception e) {
+                e.printStackTrace();
+                result = e.getMessage();
             }
             return result;
         }
--- app/src/main/java/com/roll/clientserverhttp_fragments/model/HttpProvider.java
+++ app/src/main/java/com/roll/clientserverhttp_fragments/model/HttpProvider.java
@@ -1,9 +1,57 @@
 package com.roll.clientserverhttp_fragments.model;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.util.Log;
+
+import com.roll.clientserverhttp_fragments.entities.AuthResponse;
+import com.squareup.okhttp.MediaType;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by RDL on 26/02/2017.
  */
 
 public class HttpProvider {
     public static final String BASE_URL = "https://telranstudentsproject.appspot.com/_ah/api/contactsApi/v1";
+
+    private static HttpProvider ourInstance = new HttpProvider();
+
+    public HttpProvider() {
+    }
+
+    public static HttpProvider getInstance() {
+        return ourInstance;
+    }
+
+    public String registration(String jsonBody) throws Exception {
+        String result = "";
+        MediaType mediaType = MediaType.parse("application/json; charset=utf-8");
+        RequestBody body = RequestBody.create(mediaType, jsonBody);
+        Request request = new Request.Builder()
+                .url(HttpProvider.BASE_URL + "/registration")
+                .post(body)
+                .build();
+        OkHttpClient client = new OkHttpClient();
+        client.setReadTimeout(15, TimeUnit.SECONDS);
+        client.setConnectTimeout(15, TimeUnit.SECONDS);
+        Response response = client.newCall(request).execute();
+        if (response.code() < 400) {
+            result = response.body().string();
+            Log.d("REGISTRATION", result);
+        } else if (response.code() == 409) {
+            throw new Exception("User already exist!");
+        } else {
+            String error = response.body().string();
+            Log.d("REGISTRATION ERROR", error);
+            throw new Exception("Server ERROR!");
+        }
+        return result;
+    }
 }
